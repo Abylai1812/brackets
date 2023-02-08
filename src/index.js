53ea4657764caf71bf13d098e7864686f6d80edc
Abylai <abylaikhhann@mail.ru>
2023-02-08 20:32:40 +0500

    feat:create function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  for (let i = 0; i < str.length; i++) {
+      let currChar = str[i];
+      let found = false;
+      for (let j = 0; j < bracketsConfig.length; j++) {
+          if (currChar === bracketsConfig[j][0]) {
+              stack.push(bracketsConfig[j][1]);
+              found = true;
+              break;
+          }
+          if (currChar === bracketsConfig[j][1]) {
+              if (stack.length === 0 || stack.pop() !== currChar) {
+                  return false;
+              }
+              found = true;
+              break;
+          }
+      }
+      if (!found) {
+          return false;
+      }
+  }
+  return stack.length === 0;
 }
+
